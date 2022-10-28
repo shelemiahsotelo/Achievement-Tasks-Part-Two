a99f7bf7c4007ce1ee20dc708d2d6fe1741832d9
shelemiahsotelo <115062955+shelemiahsotelo@users.noreply.github.com>
2022-10-28 12:29:43 -0400

    Added comments

--- Task#5_SquaredNumbers.py
+++ Task#5_SquaredNumbers.py
@@ -1,5 +1,10 @@
-numBase = {}
-for key in range(1,11):
-  value = key ** 3
+# Task #5 - Squared Numbers
+# Shelemiah Sotelo 8823190
+
+# Instructed to get the cubed result of numbers 1-10 using dictionary.
+
+numBase = {}                # Empty dictionary.
+for key in range(1,11):     # Range is set to 1-11 
+  value = key ** 3          # Formula for getting ^cube
   numBase[key] = value
 print(numBase)
