3ab9fa9b7229f147d51716bc0dedc5cf78e70631
shelemiahsotelo <115062955+shelemiahsotelo@users.noreply.github.com>
2022-10-31 00:58:35 -0400

    Added comments

--- Task#6_OddAndEvenNumbers.py
+++ Task#6_OddAndEvenNumbers.py
@@ -1,32 +1,38 @@
+# Achievement Task - Odd and Even Numbers
+# Shelemiah Sotelo - 8823190
+# This is to determine and separate prime and non-prime numbers.
+
+
+# Function for determining prime numbers.
 def isPrime(number):
    count = 0
 
    for i in range(2, (number//2 + 1)):
       if(number % i == 0):
-         count = count +1
+         count = count + 1
          break
    
    if (count == 0 and number != 1):
-      return True
+      return True                   # Return True if the number is prime.
    else:
-      return False
+      return False                  # Return False if the number in non-prime.
 
 
-print("Enter numbers: ") # explanation for the program
+print("Please enter any numbers. This program will distinguish what numbers are prime and non-prime. ") # explanation for the program
 numbers = input()
-numList = numbers.split(",")
+numList = numbers.split(",")       # Numbers are separated by comma. 
 
-prime = []
+prime = []                         # Empty list for prime numbers.
 
-nonPrime = []
+nonPrime = []                      # Empty list for non-prime numbers.
 
 for x in numList:
-   num = int(x)
+   num = int(x)                    # Convert into integer
    if isPrime(num):
-      prime.append(x)
+      prime.append(x)              # Add number to the prime list.
 
    else:
-      nonPrime.append(x)
+      nonPrime.append(x)           # Add number to the non-prime list.
 
 
 print("Prime numbers are", ",".join(prime))
